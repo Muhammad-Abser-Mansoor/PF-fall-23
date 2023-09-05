718e0b7e09d1f8af69e1a4c97119868946240744
Muhammad-Abser-Mansoor <142979991+Muhammad-Abser-Mansoor@users.noreply.github.com>
2023-09-05 10:37:46 +0500

    Create task1.c

---
+++ Labs/03/task1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	int l = 0,w = 0,d = 0,v = 0;
+	printf("Enter length: ");
+	scanf("%d" ,&l);
+	printf("Enter Width: ");
+	scanf("%d" ,&w);
+	printf("Enter Depth: ");
+	scanf("%d" ,&d);
+	v = l*w*d;
+	printf("Volume is %d" ,v);
+	return 1;
+}
