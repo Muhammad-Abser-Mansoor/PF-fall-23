96f8dc278298fac71917f57bb5f00053448ee404
Muhammad-Abser-Mansoor <142979991+Muhammad-Abser-Mansoor@users.noreply.github.com>
2023-09-19 21:38:39 +0500

    Update Task 10.c

--- Labs/Lab05/Task 10.c
+++ Labs/Lab05/Task 10.c
@@ -8,7 +8,7 @@ int main() {
 	scanf("%d", &bin1); // Input first half of binary number
 	printf("Enter last 4 bit binary numbers ");
 	scanf("%d", &bin2); // Input last half of binary number
-	if ((bin1 < 0000 || bin1 > 1111) || (bin2 < 0000 || bin2 > 1111) || (bin1 == 0000 && bin2 == 0000) || (bin1 == 1111 && bin2 == 1111))  {
+	if ((bin1 < 0 || bin1 > 15) || (bin2 < 0 || bin2 > 15) || (bin1 == 0000 && bin2 == 0000) || (bin1 == 1111 && bin2 == 1111))  {
 		printf("Invalid input"); } // error message for invalid input possibilities
 	else {
 		printf("New binary number is %04d%04d",bin2,bin1); } // Print swapped binary halves
