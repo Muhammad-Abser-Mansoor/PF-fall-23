adaba179b2ffa7f437341cb3878064c49beb979b
Muhammad-Abser-Mansoor <142979991+Muhammad-Abser-Mansoor@users.noreply.github.com>
2023-09-20 15:20:24 +0500

    Update Task 10.c

--- Labs/Lab05/Task 10.c
+++ Labs/Lab05/Task 10.c
@@ -1,21 +1,15 @@
-#include <stdio.h>
-#include <math.h>
+#include<stdio.h>
 
 int main() {
-	int bin1 = 0;
-	int bin2 = 0;
-	printf("Binary number must be a combination of 1s and zeros\nEnter first 4 bit binary numbers ");
-	scanf("%d", &bin1); // Input first half of binary number
-	printf("Enter last 4 bit binary numbers ");
-	scanf("%d", &bin2); // Input last half of binary number
-	if ((bin1 < 0 || bin1 > 15) || (bin2 < 0 || bin2 > 15) || (bin1 == 0000 && bin2 == 0000) || (bin1 == 1111 && bin2 == 1111))  {
-		printf("Invalid input"); } // error message for invalid input possibilities
-	else {
-		printf("New binary number is %04d%04d",bin2,bin1); } // Print swapped binary halves
+	int n = 0; // variable declaration
+	printf("Enter a 8 bit binary number ");
+	scanf("%08d", &n); // %08d to only accept 8 bit values
+	if (n <= 00000000 || n >= 11111111) {
+		printf("Invalid input");
+		return 0;
+	}
+	int rsh = n % 10000;
+	int lsh = n / 10000;
+	printf("%04d%04d",rsh,lsh);
 	return 0;
-} //End main
-
-	
-	
-		
-		
+}
