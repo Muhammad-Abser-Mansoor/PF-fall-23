be22ed6e0930a72a4d4241f7b0e519844640b909
Muhammad-Abser-Mansoor <142979991+Muhammad-Abser-Mansoor@users.noreply.github.com>
2023-09-20 15:26:31 +0500

    Update Task 10.c

--- Labs/Lab05/Task 10.c
+++ Labs/Lab05/Task 10.c
@@ -5,11 +5,11 @@ int main() {
 	printf("Enter a 8 bit binary number ");
 	scanf("%08d", &n); // %08d to only accept 8 bit values
 	if (n <= 00000000 || n >= 11111111) {
-		printf("Invalid input");
+		printf("Invalid input"); // Error for invalid input
 		return 0;
 	}
-	int rsh = n % 10000;
-	int lsh = n / 10000;
-	printf("%04d%04d",rsh,lsh);
+	int rsh = n % 10000; // separate right hand side bits of number
+	int lsh = n / 10000; // separate left hand side bits of number
+	printf("%04d%04d",rsh,lsh); // shift positions and print
 	return 0;
-}
+} // End main
