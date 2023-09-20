901ce6581e38f971806229f39f25ccd2afb32afe
Muhammad-Abser-Mansoor <142979991+Muhammad-Abser-Mansoor@users.noreply.github.com>
2023-09-20 15:29:40 +0500

    Update Task 6.c

--- Labs/Lab05/Task 6.c
+++ Labs/Lab05/Task 6.c
@@ -4,7 +4,7 @@ int main() {
 	int bin = 0;
 	printf("Enter a 8 bit binary number: ");  // Input the binary number
 	scanf("%08d",&bin);
-	if (bin < 0 && bin > 255) {
+	if (bin < 0 || bin > 255) {
 		printf("Invalid Value"); } // If any value is not binary then error message
 	else if (((bin / 100000) % 10) & ((bin / 100) % 10)) { // Checks the values at 4th and 7th bits using remainder after division by appropriate power of 10
 		bin = bin - 1000 - 1000000;
