68325dc1ae48c059701587b94d0000993db2413c
Muhammad-Abser-Mansoor <142979991+Muhammad-Abser-Mansoor@users.noreply.github.com>
2023-09-22 15:24:04 +0500

    Update parity outlier.c

--- practice/parity outlier.c
+++ practice/parity outlier.c
@@ -1,23 +1,26 @@
-#include <stdlib.h>
-
-int find_outlier(const int *values, size_t count);
+#include <stdlib.h> //COMPLETE AND WORKING
+#include <stdio.h>
+int main() {
+int n[5] = {4,6,3,2,8};
 int i = 0;
 int N = 0;
 int num = 0;
 int test = 0;
+int count = 5;
 while (i < count) {
-   test = values[i] % 2;
+   test = n[i] % 2;
   num = num + test;
   if (num = 1 && i >= 2) {
-    N = values[i];
+    N = n[i];
     printf("Outlier is %d",N);
     return 0;
   }
   else if (num > 1 && i >= 2) {
-    N = values[i];
+    N = n[i];
     printf("Outlier is %d",N);
-    return 0;
+    return 0; }
     else {
-      return 0;
+      i++;
     }
   }
+}
