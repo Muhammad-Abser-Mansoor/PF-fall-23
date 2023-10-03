79fd8ee3e60ea69c150563dd6bfa7c02ff0f4c9a
Muhammad-Abser-Mansoor <142979991+Muhammad-Abser-Mansoor@users.noreply.github.com>
2023-10-03 06:42:14 -0700

    Create L6t6.c

---
+++ Labs/Lab06/L6t6.c
@@ -0,0 +1,32 @@
+/**
+ * Programmer: Muhammad Abser Mansoor
+ * Desc: Draw sticks between computer and user but computer always wins
+ * Date: 10/3/23
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n = 21;
+    int p = 0;
+    char x = 'e';
+    while (n>0) {
+        printf("How many sticks do you want? ");
+        scanf("%d",&p);
+        n-=p;
+        if (n <= 0) {
+            printf("you lose");
+            break;
+        }
+        else {
+            n = n-(rand()%(4 - 1 + 1)) + 1;
+            if (n == 0) {
+                n=n+4;
+                continue;
+            }
+        else {
+            continue;
+        }
+        }
+    }
+}
