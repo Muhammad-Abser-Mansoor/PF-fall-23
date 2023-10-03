1e1495b2d02b60d5b20825929685e97179f63bda
Muhammad-Abser-Mansoor <142979991+Muhammad-Abser-Mansoor@users.noreply.github.com>
2023-10-03 10:45:58 +0500

    Update L6t3.c

--- Labs/Lab06/L6t3.c
+++ Labs/Lab06/L6t3.c
@@ -1,31 +1,26 @@
+/**
+ * Programmer: Muhammad Abser Mansoor
+ * Desc: Finding LCM and GCD
+ * Date: 10/3/23
+ */
 #include <stdio.h>
 
 int main() {
     int a = 0;
     int b = 0;
-    int g = 0;
     int l = 0;
     int gcd = 0;
     printf("Enter number ");
     scanf("%d", &a);
     printf("Enter number ");
     scanf("%d", &b);
-    if (a > b) {
-          g = b; 
-    }
-    else {
-         g = a; 
-    }
-    
-    while (g>0); {
-        if ((a%g==0) && (b%g==0)) {
-             gcd = g;
-             printf(("GCD is %d"), gcd);
-             break;
-         }
-       g--; 
-    }
-    l = (a*b)/gcd;
-    printf("\nLCM is %d", gcd);
-    return 0;
-}
+   for(int i = 2; i<=a && i<=b;i++) {
+        if ((a%i==0) && (b%i==0)) {
+                     gcd = i;
+                     printf(("GCD is %d"), gcd);
+                     }
+             }
+            l = (a*b)/gcd;
+            printf("\nLCM is %d", l);
+        return 0;
+        }
