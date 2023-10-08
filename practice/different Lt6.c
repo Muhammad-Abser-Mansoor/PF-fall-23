86174a63a66f7c36d0fe56d33e8e83b29bef31f9
Muhammad-Abser-Mansoor <142979991+Muhammad-Abser-Mansoor@users.noreply.github.com>
2023-10-08 15:07:08 -0700

    Update different Lt6.c

--- practice/different Lt6.c
+++ practice/different Lt6.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    int n,a;
+    int n = 0;
+    int a =0;
     int i =0;
     int ar[1000]={0};
     printf("Enter a number that is 10 or above ");
@@ -10,26 +11,42 @@ int main() {
         printf("Invalid");
     }
     while (n>=10) {
-        ar[i]=n%10;
-        n=n/10;
-        i++;
+        if (n>=10 && n<100) {
+            ar[i] = n%10;
+            printf("%d ",ar[i]);
+            i++;
+            n=n/10;
+            ar[i] = n%10;
+            printf("%d ",ar[i]);
+            n=n/10;
+        }
+            else {
+            ar[i]=n%10;
+            printf("%d ",ar[i]);
+            n=n/10;
+            i++;
+        }
     }
+    printf("\n%d",i);
     int j = 0;
-    for (i=i-1;i>=0;i--) {
-        if (ar[i]==ar[j]) {
-            continue;
+    int k = i;
+    while (k>=0) {
+        printf("\n%d %d",k,j);
+        if (ar[k]==ar[j]) {
+            printf("\n%d %d\n",ar[k],ar[j]);
+            j++;
+            k=k-1;
         }
         else {
             a++;
             break;
         }
-        j++;
     }
-    if (a != 0) {
+    if (a == 0) {
         printf("Number is a palindrome");
     }
     else {
-        printf("Number is not a plaindrome");
+        printf("\nNumber is not a plaindrome");
     }
     return 0;
 }
