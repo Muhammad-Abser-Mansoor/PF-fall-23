870c9e51e67aee0c3cda7483df1dbf6e0fabe7bc
Muhammad-Abser-Mansoor <142979991+Muhammad-Abser-Mansoor@users.noreply.github.com>
2023-10-08 12:56:43 -0700

    Update different Lt1.c

--- practice/different Lt1.c
+++ practice/different Lt1.c
@@ -10,10 +10,13 @@ int main() {
     int arr[10000]={0};
     for (int i = a;i<=b;i++) {
         arr[j]=i;
+        printf("%d ",arr[j]);
         j++;
     }
+    printf("\n");
     int check = 0;
-    while (j-1>=0) {
+    j-=1;
+    while (j>=0) {
         check = arr[j]%2;
         if (check == 0) {
             printf("%d ",arr[j]);
