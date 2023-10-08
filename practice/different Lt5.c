8835646ff41e74a315d22deddc973222991a6434
Muhammad-Abser-Mansoor <142979991+Muhammad-Abser-Mansoor@users.noreply.github.com>
2023-10-08 14:32:09 -0700

    Create different Lt5.c

---
+++ practice/different Lt5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int ar[1000]={0};
+    int n,a,d,check;
+    int j,k = 0;
+    printf("Enter size of array ");
+    scanf("%d",&n);
+    for (int i = 0;i<n;i++) {
+        printf("Enter element of array ");
+        scanf("%d",&a);
+        ar[i]=a;
+    }
+    printf("How many positions to the right do you want to shift? " );
+    scanf("%d",&d);
+    for (int i = 0;i<n;i++) {
+        if (i+d<n) {
+            check=ar[j+d];
+            printf("%d,",check);
+            j++;
+        }
+        else {
+            check=ar[k];
+            printf("%d,",check);
+            k++;
+        }
+    }
+    return 0;
+}
