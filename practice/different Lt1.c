fa6157e1a565fdc701412d5b59b43b4627578581
Muhammad-Abser-Mansoor <142979991+Muhammad-Abser-Mansoor@users.noreply.github.com>
2023-10-08 12:48:33 -0700

    Create different Lt1.c

---
+++ practice/different Lt1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    printf("Enter Start of range ");
+    scanf("%d",&a);
+    printf("Enter End of range ");
+    scanf("%d",&b);
+    int j = 0;
+    int arr[10000]={0};
+    for (int i = a;i<=b;i++) {
+        arr[j]=i;
+        j++;
+        printf("%d",arr[j]);
+    }
+    int check = 0;
+    while (j>=0) {
+        check = arr[j]%2;
+        if (check == 0) {
+            printf("%d ",arr[j]);
+            j--;
+        }
+        else {
+            j--;
+        }
+    }
+    return 0;
+}
