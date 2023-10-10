d5549c4cac0b0a004e0381d5f9ce1924be012c5b
Muhammad-Abser-Mansoor <142979991+Muhammad-Abser-Mansoor@users.noreply.github.com>
2023-10-10 09:18:14 +0500

    Create Task5.c

---
+++ Labs/Lab07/Task5.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+int main() {
+    int i=0;
+        while (i<3) {
+            printf("\t\t*****\t\t");
+            i++;
+        }
+        while (i==3) {
+            printf("*********************");
+            i++;
+        }
+        while (i==4) {
+            printf("\t\t*\t*\t\t");
+            i++;
+        }
+        while (i==5) {
+            printf("\t\t\t|\t\t\t");
+            i++;
+        }
+        while (i==6) {
+            printf("\t\t\\__/\t\t");
+            i++;
+        }
+        while (i==7) {
+            printf("\n");
+            i++;
+        }
+        while (i==8) {
+            printf("\t\t|\t|\t\t");
+            i++;
+        }
+        while (i==9) {
+            printf("*********************");
+            i++;
+        }
+        while (i<14) {
+            printf("**\t*****\t**");
+            i++;
+        }
+        while (i==14) {
+            printf("\t\t*****\t\t");
+            i++;
+        }
+        while (i<20) {
+            printf("\t\t**\t**\t\t");
+            i++;
+        }
+        while (i==20) {
+            printf(" *****\t***** ");
+            
+            i++
+        }
+    return 0;
+} //end main()
