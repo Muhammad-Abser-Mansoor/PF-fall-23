01c2f10cd8773e7017a928305550d88c9081652a
Muhammad-Abser-Mansoor <142979991+Muhammad-Abser-Mansoor@users.noreply.github.com>
2023-10-24 10:40:51 +0500

    Create Task7.c

---
+++ Labs/Lab08/Task7.c
@@ -0,0 +1,39 @@
+/* 
+  * Programmer: Muhammad Abser Mansoor
+  * Date: 24/10/2023
+  * Desc: Do operations on an array
+  */
+
+#include <stdio.h>
+#include <string.h>
+
+ /* 
+  * Programmer: Muhammad Abser Mansoor
+  * Date: 24/10/2023
+  * Desc: Find Max
+  */
+double calculateDiscount(int times,double price) {
+    double discount = 0;
+    if (times > 10 && price >= 50) {
+        discount = price - (price*0.15);
+        return discount;
+    }
+    else if (times > 5 && price >= 30) {
+        discount = price - (price*0.1);
+        return discount;
+    }
+    else {
+        discount = 0;
+        return discount;
+    }
+}
+int main() {
+    int ti=0;
+    double pr=0;
+    printf("Enter how many times you visited in this month ");
+    scanf("%d",&ti);
+    printf("Enter total price ");
+    scanf("%lf",&pr);
+    printf("%lf",calculateDiscount(ti,pr));
+    return 0;
+}
