84d745506ab599507211713c40e0bc70adf030e8
Muhammad-Abser-Mansoor <142979991+Muhammad-Abser-Mansoor@users.noreply.github.com>
2023-10-24 19:37:41 +0500

    Update Q2.c

--- assignments/Race02/Q2.c
+++ assignments/Race02/Q2.c
@@ -5,33 +5,53 @@ int main() {
     int one[100][100];
     int a,b,c;
     int avg,max,fif,hun,sum=0;
-    printf("Enter breadth of matrix ");
-    scanf("%d",&a);
-    printf("Enter length of matrix ");
+    printf("Enter columns of matrix ");
     scanf("%d",&b);
+    printf("Enter rows of matrix ");
+    scanf("%d",&a);
     for (int i = 0;i<a;i++) {
         for (int j = 0;j<b;j++) {
             printf("Enter 1 or 0 in matrix position (%d,%d) ",i,j);
             scanf("%d",&ar[i][j]);
-            if (ar[i][j] != 0 || ar[i][j] != 1) {
+            if (ar[i][j] != 0 && ar[i][j] != 1) {
                 printf("Ya done fked up");
                 break;
             }
         }
     }
-    for (int i = 2;i<a;i++) {
-        for (int j = 0;j<a-i+1;j=j+i) {
-            for (int k = 0;k<b-i+1;k=k+i) {
+    int p=0;
+    if (a>b) {
+        p = b;
+    }
+    else if (b>a) {
+        p = a;
+    }
+    else if (b == a) {
+        p = a;
+    }
+    int m,n = 0;
+    for (p = a;p>=2;p--) {
+        m,n=0;
+        for (int j = 0;j<a-p+1;j=j+p) {
+            for (int k = 0;k<b-p+1;k=k+p) {
                 if (ar[j][k] != 1) {
-                    k = b-i;
+                    k = b-p;
                 }
                 else {
                     ar[j][k] = one[m][n];
                     n++;
+                    if (n == p-1 && m != p-1) {
+                        n = 0;
+                        m++;
+                    }
                 }
             }
             
         }
+        if (m == a-1 && n == a-1) {
+            break;
+        }
     }
+    printf("largest submatrix of 1s is a %dx%d",a,a);
     return 0;
 }
