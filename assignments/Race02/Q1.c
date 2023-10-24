8f3f800cd14f0fe5bd0454c0a43738b1d0d9e2d3
Muhammad-Abser-Mansoor <142979991+Muhammad-Abser-Mansoor@users.noreply.github.com>
2023-10-24 13:29:32 +0500

    Update Q1.c

--- assignments/Race02/Q1.c
+++ assignments/Race02/Q1.c
@@ -7,18 +7,19 @@
 
 int main() {
     int ar[100][100];
-    int a,c;
+    int a,b,c;
     int avg,max,fif,hun,sum=0;
     printf("Enter total number of batsmen ");
     scanf("%d",&a);
     for (int i = 0;i<a;i++) {
         avg,max,fif,hun,sum=0;
         printf("Enter number of batsman %d ",i+1);
-        scanf("%d",&ar[i][0]);
+        scanf("%d",&b);
+        ar[i][0] = b;
         printf("Enter total number of innings for batsman %d ",i+1);
         scanf("%d",&c);
-        for (int j = 0;j<c;j++) {
-            printf("Enter runs in inning %d ",j+1);
+        for (int j = 1;j<=c;j++) {
+            printf("Enter runs in inning %d ",j);
             scanf("%d",&ar[i][j]);
             if (max<ar[i][j]) {
                 max = ar[i][j];
@@ -35,4 +36,4 @@ int main() {
         printf("Statistics of Batsmen with id %d:\nHighest score in a single inning:%d\nNumber of fifties:%d\nNumber of hundreds:%d\nAverage runs in innings:%d\nTotal runs scored:%d\n",ar[i][0],max,fif,hun,avg,sum);
     } 
     return 0;
-} //end main
+}
