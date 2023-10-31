d8dfc0504b711a578fb2b6cfa451fdad64a636cd
Muhammad-Abser-Mansoor <142979991+Muhammad-Abser-Mansoor@users.noreply.github.com>
2023-10-31 10:09:07 +0500

    Create Task1.c

---
+++ Labs/Lab09/Task1.c
@@ -0,0 +1,21 @@
+/* Programmer:Muhammad Abser Mansoor
+* Date: 31/10/23
+* Descrption:swaping two variables using array
+*/
+
+#include<stdio.h>
+
+void swap(int *a, int *b){
+	*a = *b;
+	int tmp = *a;
+	*b = tmp;
+	
+}
+
+int main(){
+	int j = 2, k = 5;
+	printf("j=%d, k=%d\n", j,k);
+	swap(&j,&k);
+	printf("j=%d, k=%d\n", j,k);
+	return 0;
+}
