5ea8be18ff2c721de6675e04031d41397bb41293
Muhammad-Abser-Mansoor <142979991+Muhammad-Abser-Mansoor@users.noreply.github.com>
2023-11-07 20:57:59 +0500

    Create q8.c

---
+++ assignments/Race02/q8.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/* Programmer: Abser Mansoor
+ * Date: 28/10/2023
+ * Desc.: Find the persistence of continous numbers until EOF is entered
+ */
+int main() {
+    int n;
+    int orig;
+    int a =0;
+    char x;
+    for (;;) {
+        printf("Enter n for which persistence will be found ");
+        scanf("%d",&n);
+        orig = n;
+        while (n>9) {
+            int num = 1;
+            for (int j = 0;n>0;n = n/10) {
+            num = num*n%10;
+            n/=10;
+        }
+        n = num;
+        a++;
+        }
+        printf("persistence of %d is %d\n",n,a);
+        printf("Is that all? y/n ");
+        scanf(" %c",&x);
+        if (x == 'y') {
+            return;
+        }
+    }
+}
